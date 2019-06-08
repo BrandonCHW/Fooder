5f207c6e9bc6f7300aaf7b386069aa4c24993602
BrandonCHW <chanhingwah.brandon@gmail.com>
2019-06-08 19:50:01 -0400

    wip get lat long

--- App.js
+++ App.js
@@ -10,25 +10,38 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import { GetMyLocation } from './components/GetMyLocation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <GetMyLocation onGetLocation={this.handleFetchLocation}></GetMyLocation>
+        <Text>Latitude: {this.latitude}</Text>
+        <Text>Longitude: {this.longitude}</Text>
       </View>
     );
   }
 
+  changeCoords(lat: number, long: number) {
+    this.setState({
+      latitude: lat,
+      longitude: long
+    })
+  }
+
   handleFetchLocation = () => {
     console.log("get my location pressed");
-    navigator.geolocation.getCurrentPosition((pos) => console.log(pos), (err) => console.log("couldn't get location"));
+    navigator.geolocation.getCurrentPosition((pos) => {
+      this.changeCoords(pos.coords.latitude, pos.coords.longitude);
+      console.log(pos);
+    }, (err) => console.log("couldn't get location"));
   }
 }
 
