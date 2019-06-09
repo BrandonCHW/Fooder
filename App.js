4906b7ba90a6e79622c7cdc1375c30cd0c2aaf85
BrandonCHW <chanhingwah.brandon@gmail.com>
2019-06-08 23:58:32 -0400

    add view for locaiton (lat and long)

--- App.js
+++ App.js
@@ -6,60 +6,68 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
-import { GetMyLocation } from './components/GetMyLocation';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { GetMyLocation } from "./components/GetMyLocation";
 
-export default class App extends Component<{}> {
-  constructor() {
-    super();
-    this.state = {
-      latitude: 0,
-      longitude: 0
-    }
-  }
+type Props = {};
+type State = {
+	latitude: number,
+	longitude: number
+};
+export default class App extends Component<Props, State> {
+	constructor() {
+		super();
+		this.state = {
+			latitude: 0,
+			longitude: 0
+		};
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <GetMyLocation onGetLocation={this.handleFetchLocation}></GetMyLocation>
-        <Text>Latitude: {this.state.latitude}</Text>
-        <Text>Longitude: {this.longitude}</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<GetMyLocation onGetLocation={this.handleFetchLocation} />
+				<Text>Latitude: {this.state.latitude}</Text>
+				<Text>Longitude: {this.state.longitude}</Text>
+			</View>
+		);
+	}
 
-  changeCoords(lat: number, long: number) {
-    this.setState({
-      latitude: lat,
-      longitude: long
-    })
-  }
+	changeCoords(lat: number, long: number) {
+		this.setState({
+			latitude: lat,
+			longitude: long
+		});
+	}
 
-  handleFetchLocation = () => {
-    console.log("get my location pressed");
-    navigator.geolocation.getCurrentPosition((pos) => {
-      this.changeCoords(pos.coords.latitude, pos.coords.longitude);
-      console.log(pos);
-    }, (err) => console.log("couldn't get location"));
-  }
+	handleFetchLocation = () => {
+		console.log("get my location pressed");
+		navigator.geolocation.getCurrentPosition(
+			pos => {
+				this.changeCoords(pos.coords.latitude, pos.coords.longitude);
+				console.log(pos);
+			},
+			err => console.log("couldn't get location")
+		);
+	};
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#B21E5E',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#B21E5E"
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: "center",
+		margin: 10
+	},
+	instructions: {
+		textAlign: "center",
+		color: "#333333",
+		marginBottom: 5
+	}
 });
